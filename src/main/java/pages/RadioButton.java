e0a57d11c5a75a4f35a36e0aa8abc7326fa403c2
HazimOkanovic <62857146+HazimOkanovic@users.noreply.github.com>
2022-02-03 09:38:39 +0100

    Update RadioButton.java

--- src/main/java/pages/RadioButton.java
+++ src/main/java/pages/RadioButton.java
@@ -7,20 +7,30 @@ import org.openqa.selenium.interactions.Actions;
 
 public class RadioButton {
     private WebDriver driver;
+    private By yesButton = By.id("yesRadio");
+    private By impressiveButton = By.id("impressiveRadio");
+    private By textField = By.xpath("//p[@class='mt-3']");
+    private By noButton = By.xpath("//input[@id='noRadio']");
+    
     public RadioButton (WebDriver driver){
         this.driver = driver;
     }
     public void clickYes(){
         Actions actions = new Actions(driver);
-        WebElement element = driver.findElement(By.id("yesRadio"));
+        WebElement element = driver.findElement(yesButton);
         actions.click(element).perform();
     }
     public void clickImpressive(){
         Actions actions = new Actions(driver);
-        WebElement element = driver.findElement(By.id("impressiveRadio"));
+        WebElement element = driver.findElement(impressiveButton);
+        actions.click(element).perform();
+    }
+    public void clickNoButton(){
+        Actions actions = new Actions(driver);
+        WebElement element = driver.findElement(noButton);
         actions.click(element).perform();
     }
     public String checkResult(){
-        return driver.findElement(By.xpath("//p[@class='mt-3']")).getText();
+        return driver.findElement().getText(textField);
     }
 }
