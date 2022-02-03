ec9df2944ce4e57a21547853dc44769d740571b0
HazimOkanovic <62857146+HazimOkanovic@users.noreply.github.com>
2022-02-03 09:31:01 +0100

    Update RadioButtonTest.java

--- src/test/java/radiobutton/RadioButtonTest.java
+++ src/test/java/radiobutton/RadioButtonTest.java
@@ -6,15 +6,30 @@ import static org.testng.Assert.assertEquals;
 
 public class RadioButtonTest extends BaseTests {
     @Test
-    public void radioButtonTest(){
+    public void yesButtonTest(){
         var elementsPage = homePage.clickElements();
         var radioPage = elementsPage.clickRadioButton();
         String selectedYes = "You have selected Yes";
-        String selectedImpressive = "You have selected Impressive";
-
+      
         radioPage.clickYes();
-        assertEquals(radioPage.checkResult(), selectedYes, "Yes is not working");
+        assertEquals(radioPage.checkResult(), selectedYes, "Yes is not working");     
+    }
+    @Test
+    public void impressiveButtonTest(){
+        var elementsPage = homePage.clickElements();
+        var radioPage = elementsPage.clickRadioButton();
+        String selectedImpressive = "You have selected Impressive";
+        
         radioPage.clickImpressive();
         assertEquals(radioPage.checkResult(), selectedImpressive, "Impressive is not working");
-    }
+    }   
+     @Test
+    public void noButtonTest(){
+        var elementsPage = homePage.clickElements();
+        var radioPage = elementsPage.clickRadioButton();
+        String selectedNoButton = "You have selected No Button";
+        
+        radioPage.clickImpressive();
+        assertEquals(radioPage.checkResult(), selectedNoButton, "No Button is not working");
+    }   
 }
